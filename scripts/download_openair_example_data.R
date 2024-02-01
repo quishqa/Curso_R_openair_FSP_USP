43fa254209486a47653e4a697b3d78df5d7fd3f9
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2024-02-01 19:06:56 -0300

    fix: update sample openair data

--- scripts/download_openair_example_data.R
+++ scripts/download_openair_example_data.R
@@ -1,9 +1,10 @@
 # Downloading sample data for openair presentation
 
 library(qualR)
+library(openair)
 
 start <- "01/01/2019"
-end <- "31/12/2019"
+end <- "31/12/2021"
 params <- c("VV", "DV", "MP2.5", "O3", "NO2", "NO")
 
 pin <- cetesb_retrieve_param(username = Sys.getenv("QUALAR_USER"),
@@ -12,4 +13,6 @@ pin <- cetesb_retrieve_param(username = Sys.getenv("QUALAR_USER"),
                              aqs_code = "Pinheiros",
                              start_date = start,
                              end_date = end)
+
+pin <- selectByDate(pin, year = 2019:2021)
 saveRDS(pin, file="data/pin_openair_ex.rds")
